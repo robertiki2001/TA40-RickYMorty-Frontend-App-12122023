3e224559f9e71b19c823d6ae316c177d6fe5a920
robertiki2001 <robertrlm16@gmail.com>
2023-12-13 20:32:21 +0100

    Arreglando Parte 2 (Funcionamiento correcto)

--- src/app/components/characters/character-card/character-card.component.ts
+++ src/app/components/characters/character-card/character-card.component.ts
@@ -1,20 +1,18 @@
-import { Input, OnInit } from '@angular/core';
-import { NgModule } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
 
-@NgModule({
-  declarations: [CharacterCardComponent],
-  imports: [CommonModule],
-  exports: [CharacterCardComponent],
+@Component({
+  selector: 'app-character-card',
+  standalone: true,
+  imports: [ CommonModule, RouterLink],
+  templateUrl: './character-card.component.html',
+  styleUrls: ['./character-card.component.css']
 })
-
 export class CharacterCardComponent implements OnInit {
-
   @Input() character: any;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
--- src/app/components/characters/characters-list/characters-list.component.ts
+++ src/app/components/characters/characters-list/characters-list.component.ts
@@ -2,25 +2,26 @@ import { Component } from '@angular/core';
 import { CharactersService } from '../../../services/characters.service';
 import { CharacterCardComponent } from '../../../components/characters/character-card/character-card.component';
 import { OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 
 @Component({
   selector: 'app-characters-list',
   standalone: true,
-  imports: [CharacterCardComponent],
+  imports: [CharacterCardComponent, CommonModule],
   templateUrl: './characters-list.component.html',
   styleUrl: './characters-list.component.css'
 })
 export class CharactersListComponent implements OnInit{
 
-  personajes: any;
+  characters: any;
 
   constructor(private charactersService: CharactersService) {}
 
   ngOnInit(): void {
     this.charactersService.getCharacters().subscribe({
       next: (result) => {
-        this.personajes = result;
+        this.characters = result;
       },
       error: (error) => {
         console.log("Problemas", error);
